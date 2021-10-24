1bbf6061a3e331d514fff4b946a86da537884116
Zachary <zach.gibbs@gmail.com>
2021-10-24 14:09:12 -0400

    Working on the JSON

--- performancetoday/main.py
+++ performancetoday/main.py
@@ -1,4 +1,20 @@
 def main():
+
+    import requests
+    from bs4 import BeautifulSoup
+    import json
+    import pprint
+    url = 'https://www.yourclassical.org/performance-today'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    Information = soup.find(id='__NEXT_DATA__').string
+    y= json.loads(Information)
+    pprint.pprint(y)
+
+
+    #print(Information)
+    #Such pretty soup
+    #print(soup.prettify())
     pass
 
 
